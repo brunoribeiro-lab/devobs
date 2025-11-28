41c4bf05ed581707ad0998a803676c5c1daa5175
Bruno Ribeiro <bruno.ribeiro.lab@gmail.com>
2025-11-27 23:53:29 -0300

    Add: 404 Invalid Endpoint

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -2,6 +2,4 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::fallback(fn() => response()->json(['error' => 'invalid endpoint'], 404));
